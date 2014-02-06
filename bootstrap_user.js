c37bdcfb5145e753e3066ad49ec6b4713355cd95
Jeff Kingyens <jeff.kingyens@gmail.com>
2014-02-05 18:10:47 -0800

    more fixes

--- bootstrap_user.js
+++ bootstrap_user.js
@@ -3,18 +3,22 @@
 var ghost = require('./core');
 ghost();
 
+setTimeout(function () {
 
-// inject an admin user from environment variables
-var api = require('./core/server/api');
-api.users.add({
-  name: process.env.user_name,
-  email: process.env.user_email,
-  password: process.env.user_password
-}).then(function (user) {
-  console.log('added user');
-  console.log(user);
-  process.exit(0);
-}).otherwise( function (err) {
-  console.log(err);
-  process.exit(-1);
-});
+  // inject an admin user from environment variables
+  var api = require('./core/server/api');
+  api.users.add({
+    name: process.env.user_name,
+    email: process.env.user_email,
+    password: process.env.user_password
+  }).then(function (user) {
+    console.log('added user');
+    console.log(user);
+    process.exit(0);
+  }).otherwise( function (err) {
+    console.log(err);
+    process.exit(-1);
+  });
+
+}, 10000);
+
