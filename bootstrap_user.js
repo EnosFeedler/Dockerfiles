67bb670665c90a5316b7374690d4ec1dadd00885
Jeff Kingyens <jeff.kingyens@gmail.com>
2014-02-05 17:59:43 -0800

    load ghost core before using api

--- bootstrap_user.js
+++ bootstrap_user.js
@@ -1,8 +1,18 @@
-var api = require('./core/server/api');
+
+// initialize ghost
+var ghost = require('./core');
+ghost();
 
 // inject an admin user from environment variables
 api.users.add({
   name: process.env.user_name,
   email: process.env.user_email,
   password: process.env.user_password
+}).then(function (user) {
+  console.log('added user');
+  console.log(user);
+  process.exit(0);
+}).otherwise( function (err) {
+  console.log(err);
+  process.exit(-1);
 });
